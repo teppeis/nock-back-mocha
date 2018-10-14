cd35b7b2d9f898f7f2bcfcdf1cad8d688fcf9f19
Teppei Sato <teppeis@gmail.com>
2018-10-14 15:20:49 +0900

    feat: auto detect fixtures dir, support nested fixtures dirs and expose nock.back.fixtureFile (#3)
    - feat: auto detect fixtures dir
    - feat: make nested fixture dirs for mocha's nested BDD spec cases
    - feat: expose `nock.back.fixtureFile` in each test
    - docs: add examples

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
 'use strict';
 
+const path = require('path');
 const nock = require('nock');
 const sanitize = require('sanitize-filename');
 
-module.exports = fixtures => {
-  const filenames = [];
+function defaultFixtures() {
+  return path.join(path.dirname(module.parent.filename), '__nock_fixtures__');
+}
+
+module.exports = (fixtures = defaultFixtures()) => {
+  const filenames = new Set();
   return {
-    beforeEach(done) {
-      const filename = sanitize(`${this.currentTest.title}.json`);
+    beforeEach() {
+      const filename = `${path.join(...this.currentTest.titlePath().map(sanitize))}.json`;
       // make sure we're not reusing the nock file
-      if (filenames.indexOf(filename) !== -1) {
-        return done(
+      if (filenames.has(filename)) {
+        return Promise.reject(
           new Error(
             `nock-back-mocha does not support multiple tests with the same name. ${filename} cannot be reused.`
           )
         );
       }
-      filenames.push(filename);
+      filenames.add(filename);
+      nock.back.fixtureFile = path.join(fixtures, filename);
 
       const previousFixtures = nock.back.fixtures;
       nock.back.fixtures = fixtures;
 
-      nock.back(filename, nockDone => {
-        this.currentTest.nockDone = function() {
+      return nock.back(filename).then(({nockDone, context}) => {
+        this.currentTest.nockDone = () => {
           nockDone();
+          context.assertScopesFinished();
           nock.back.fixtures = previousFixtures;
+          delete nock.back.fixtureFile;
         };
-        done();
       });
     },
     afterEach() {
       this.currentTest.nockDone();
+      delete this.currentTest.nockDone;
     },
   };
 };
